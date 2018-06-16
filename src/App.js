92cf034b950356a4626595137bb71ee407db4c9f
Matthew Hemingway <dpplrdr435@gmail.com>
2018-06-15 21:12:55 -0400

    Added sub-fetches of tracks to each playlist

--- src/App.js
+++ src/App.js
@@ -16,30 +16,6 @@ let fakeServerData = {
           {name: 'I am one', duration: 1236},
           {name: 'Jimmy Dance', duration: 70000}
         ]
-      },
-      {
-        name: 'Discover Weekly',
-        songs: [
-          {name: 'Beat It', duration: 1345},
-          {name: 'I am one', duration: 1236},
-          {name: 'Jimmy Dance', duration: 70000}
-        ]
-      },
-      {
-        name: 'Another playlist - the best!',
-        songs: [
-          {name: 'Beat It', duration: 1345},
-          {name: 'I am one', duration: 1236},
-          {name: 'Jimmy Dance', duration: 70000}
-        ]
-      },
-      {
-        name: 'Playlist!',
-        songs: [
-          {name: 'Beat It', duration: 1345},
-          {name: 'I am one', duration: 1236},
-          {name: 'Jimmy Dance', duration: 70000}
-        ]
       }
     ]
   }
@@ -119,13 +95,37 @@ class App extends Component {
       headers: {'Authorization': `Bearer ${accessToken}`}
     })
     .then(response => response.json())
-    .then(data => this.setState({
-      playlists: data.items.map(item => {
-        console.log(data.items);
+    .then(playlistData => {
+      let playlists = playlistData.items;
+      let trackDataPromises = playlists.map(playlist => {
+        let responsePromise = fetch(playlist.tracks.href, {
+          headers: { 'Authorization': `Bearer ${accessToken}` }
+        })
+        let trackDataPromise = responsePromise
+          .then(response => response.json())
+        return trackDataPromise
+      })
+      let allTracksDataPromises = 
+        Promise.all(trackDataPromises)
+      let playlistsPromise = allTracksDataPromises.then(trackDatas => {
+        trackDatas.forEach((trackData, i) => {
+          playlists[i].trackDatas = trackData.items
+          .map(item => item.track)
+          .map(trackData => ({
+            name: trackData.name,
+            duration: trackData.duration_ms / 1000
+          }))
+        })
+        return playlists
+      })
+      return playlistsPromise
+    })
+    .then(playlists => this.setState({
+      playlists: playlists.map(item => {
         return {
           name: item.name,
           imageUrl: item.images[0].url,
-          songs: []
+          songs: item.trackDatas.slice(0, 3)
         }
       })
     }))
